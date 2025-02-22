f9a036b8e1257df1a63a12f82574877dd15462e0
Manuel Cabras <9106069+2lazy2debug@users.noreply.github.com>
2025-02-23 00:10:04 +0100

    create tasks for vscode, boilerplate files and folders

---
+++ build/CMakeFiles/SimplePlotter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimplePlotter.dir/main.cpp.o"
+  "CMakeFiles/SimplePlotter.dir/main.cpp.o.d"
+  "SimplePlotter"
+  "SimplePlotter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimplePlotter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
